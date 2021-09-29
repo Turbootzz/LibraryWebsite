9c8d6a5b45562865170ccf6fe0b8bf5b728ceb12
Thijs Herman <tc.herman@hotmail.com>
2021-09-29 16:54:02 +0200

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2021 Het boekenarchief |<a href="https://phpgurukul.com/" target="_blank"  > Made by : Thijs Herman</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
